871e9fa2d9f8d5983daf793ec6ad02ef12629d79
haru2036 <hogehoge111@gmail.com>
2012-08-13 17:32:09 +0900

    first commit

---
+++ kinectTest1/Form1.Designer.cs
@@ -0,0 +1,126 @@
+﻿namespace kinectTest1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージ リソースが破棄される場合 true、破棄されない場合は false です。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.panel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("MS UI Gothic", 25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.label1.Location = new System.Drawing.Point(3, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(208, 34);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "見つかりません";
+            // 
+            // panel1
+            // 
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1087, 202);
+            this.panel1.TabIndex = 1;
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.button3);
+            this.panel2.Controls.Add(this.button2);
+            this.panel2.Controls.Add(this.button1);
+            this.panel2.Controls.Add(this.label1);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel2.Location = new System.Drawing.Point(0, 0);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(1087, 206);
+            this.panel2.TabIndex = 2;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(568, 167);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "cutoff";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(649, 167);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "resonance";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(730, 167);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 1;
+            this.button3.Text = "type";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1087, 202);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ kinectTest1/Form1.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using Microsoft.Kinect;
+using Midi;
+namespace kinectTest1
+{
+    public partial class Form1 : Form
+    {
+        OutputDevice outputDevice = OutputDevice.InstalledDevices[1];
+        KinectSensor kinect;
+        static float X, Y, Z;
+        public Form1()
+        {
+            InitializeComponent();
+            outputDevice.Open();
+            try
+            {
+                if (KinectSensor.KinectSensors.Count == 0)
+                {
+                    throw new Exception("Kinectが接続されていません");
+                }
+
+
+                // Kinectインスタンスを取得する
+                kinect = KinectSensor.KinectSensors[0];
+
+                // すべてのフレーム更新通知をもらう
+                kinect.AllFramesReady += new EventHandler<AllFramesReadyEventArgs>(kinect_AllFramesReady);
+
+                // Color,Depth,Skeletonを有効にする
+                kinect.SkeletonStream.Enable();
+
+                // Kinectの動作を開始する
+                kinect.Start();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+                Close();
+            }
+        }
+
+        // すべてのデータの更新通知を受け取る
+        void kinect_AllFramesReady(object sender, AllFramesReadyEventArgs e)
+        {
+
+            // 骨格位置の表示
+            ShowSkeleton(e);
+        }
+
+        private void ShowSkeleton(AllFramesReadyEventArgs e)
+        {
+
+            // スケルトンフレームを取得する
+            SkeletonFrame skeletonFrame = e.OpenSkeletonFrame();
+            if (skeletonFrame != null)
+            {
+                // スケルトンデータを取得する
+                Skeleton[] skeletonData = new Skeleton[skeletonFrame.SkeletonArrayLength];
+                skeletonFrame.CopySkeletonDataTo(skeletonData);
+
+                // プレーヤーごとのスケルトンを描画する
+                foreach (var skeleton in skeletonData)
+                {
+                    if (skeleton.TrackingState == SkeletonTrackingState.Tracked)
+                    {
+                          // 右手の座標を表示
+                        foreach (Joint joint in skeleton.Joints)
+                        {
+                            if (joint.JointType == JointType.HandRight)
+                            {
+                                label1.Text = "(" + joint.Position.X.ToString() + "," + joint.Position.Y.ToString() + "," + joint.Position.Z.ToString() + ")";
+                                X = joint.Position.X;
+                                Y = joint.Position.Y;
+                                Z = joint.Position.Z;
+                                int vx, vy, vz;
+                                vx = Math.Min(127,(int)(63.5*(X+1)));
+                                vy = Math.Min(127,(int)(63.5 * (Y + 1)));
+                                //vz = Math.Min(127,(int)(63.5 * (Z -3)));
+                                int cutoff, type,reso;
+                                //type = vz;
+                                cutoff = vx;
+                                reso = vy;
+
+
+                                outputDevice.SendControlChange(Channel.Channel1, Midi.Control.ModulationWheel, cutoff);
+                                //outputDevice.SendControlChange(Channel.Channel2, Midi.Control.ModulationWheel, type);
+                                outputDevice.SendControlChange(Channel.Channel3, Midi.Control.ModulationWheel, reso); 
+                            }
+                        }
+
+
+                        
+
+                        }
+                    }
+                }
+            }
+
+      
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            outputDevice.Close();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            outputDevice.SendControlChange(Channel.Channel2, Midi.Control.ModulationWheel, 0);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            outputDevice.SendControlChange(Channel.Channel1, Midi.Control.ModulationWheel, 0);
+            
+            
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            outputDevice.SendControlChange(Channel.Channel3, Midi.Control.ModulationWheel, 0); 
+        }
+
+
+        }
+    }
